57b361570815b93faecf1c81a4ef435d38220b08
GrowlingM1ke <michallange1995@gmail.com>
2020-04-01 01:21:54 +0200

    Added parallaxing and began work on tiling

--- Assets/Parallaxing.cs
+++ Assets/Parallaxing.cs
@@ -35,7 +35,15 @@ public class Parallaxing : MonoBehaviour
     {
         for (int i = 0; i < backgrounds.Length; i++)
         {
+            float parallax = (previousCamPos.x - cam.position.x) * parallaxScales[i];
 
+            float backgroundTargetPosX = backgrounds[i].position.x + parallax;
+
+            Vector3 backgroundTargetPos = new Vector3(backgroundTargetPosX, backgrounds[i].position.y, backgrounds[i].position.z);
+
+            backgrounds[i].position = Vector3.Lerp(backgrounds[i].position, backgroundTargetPos, smoothing * Time.deltaTime);
         }
+
+        previousCamPos = cam.position;
     }
 }
---
+++ Assets/Tiling.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(SpriteRenderer))]
+
+public class Tiling : MonoBehaviour
+{
+    public int offsetX = 2;
+
+    public bool hasARightBuddy = false;
+    public bool hasALeftBuddy = false;
+
+    public bool reverseScale = false;
+
+    private float spriteWidth = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
