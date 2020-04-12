64d1f64478e98ac8668b58bdf796fec4b896c937
GrowlingM1ke <michallange1995@gmail.com>
2020-04-12 17:26:43 +0200

    Rearranged files in project

---
+++ Assets/Scripts/Cutscenes_Menus/mainmenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class mainmenu : MonoBehaviour
+{
+    public Sprite first;
+    public Sprite second;
+    int state = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, 0 << LayerMask.NameToLayer("Default"));
+
+        if (hit.collider != null && state == 1)
+        {
+            GetComponent<Image>().sprite = second;
+            state = 2;
+        }
+        else if (hit.collider == null && state == 2)
+        {
+            GetComponent<Image>().sprite = first;
+            state = 1;
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Mouse0) == true && state == 2)
+        {
+            Destroy(GameObject.Find("Muzyka"));
+            SceneManager.LoadScene("Prolog _cutscena");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+
+    }
+
+
+}
