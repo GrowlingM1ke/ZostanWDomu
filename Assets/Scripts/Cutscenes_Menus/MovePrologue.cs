6712c417528de291797355bf2e17038e0c223d81
GrowlingM1ke <michallange1995@gmail.com>
2020-04-13 15:54:38 +0200

    Fixed prologue cutscene

--- Assets/Scripts/Cutscenes_Menus/MovePrologue.cs
+++ Assets/Scripts/Cutscenes_Menus/MovePrologue.cs
@@ -2,18 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
 public class MovePrologue : MonoBehaviour
 {
-
+    public Camera cam;
     float time = 0;
-    // Update is called once per frame
-    void FixedUpdate()
+    float spriteWidth;
+    float spriteHeight;
+    public RectTransform rt;
+
+
+
+    void Update()
     {
+
+        Vector3[] v = new Vector3[4];
+        rt.GetWorldCorners(v);
+        float camHorizontalExtend = cam.orthographicSize * Screen.width / Screen.height;
+        float edgeVisiblePositionRight = v[3].x - camHorizontalExtend;
+
+
         time += Time.deltaTime;
-        if (time > 13f && gameObject.transform.position.x > -17)
+        if (time > 13f && edgeVisiblePositionRight >= cam.transform.position.x)
         {
-            gameObject.transform.position = new Vector3(gameObject.transform.position.x - 6f * Time.deltaTime, gameObject.transform.position.y, gameObject.transform.position.z);
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x - 9f * Time.deltaTime, gameObject.transform.position.y, gameObject.transform.position.z);
 
         }
 
